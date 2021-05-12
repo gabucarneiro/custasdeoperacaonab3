5cd17a317e0082ee309205c2768b580b85d6875d
Gabriel Carneiro <gabucarneiro@gmail.com>
2021-05-11 22:54:07 -0300

    Ajustes para evitar crashes na activity de cryptos.

--- app/src/main/java/com/example/custasnasopb3/Crypto.java
+++ app/src/main/java/com/example/custasnasopb3/Crypto.java
@@ -65,15 +65,37 @@ public class Crypto extends AppCompatActivity {
     public void Buy (View view){
         Market market = new Market();
 
-        double calculo = market.Buy(Double.parseDouble(((EditText) findViewById(R.id.edBuyCoinDisponivel)).getText().toString()), Double.parseDouble(((EditText) findViewById(R.id.edBuyGoalCoinValor)).getText().toString()), 0.1);
-        ((TextView) findViewById(R.id.edBuyGoalCoinQuantidade)).setText(String.valueOf(df4.format(calculo)));
-        Toast.makeText(this, String.valueOf(calculo), Toast.LENGTH_SHORT).show();
+        try {
+            double calculo = market.Buy(Double.parseDouble(((EditText) findViewById(R.id.edBuyCoinDisponivel)).getText().toString()), Double.parseDouble(((EditText) findViewById(R.id.edBuyGoalCoinValor)).getText().toString()), 0.1);
+            ((TextView) findViewById(R.id.edBuyGoalCoinQuantidade)).setText(String.valueOf(df4.format(calculo)));
+            Toast.makeText(this, String.valueOf(calculo), Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e){
+            if(((EditText) findViewById(R.id.edBuyCoinDisponivel)).getText().toString().equals("")){
+                ((EditText) findViewById(R.id.edBuyCoinDisponivel)).setError("Valor requerido");
+            }
+            if(((EditText) findViewById(R.id.edBuyGoalCoinValor)).getText().toString().equals("")){
+                ((EditText) findViewById(R.id.edBuyGoalCoinValor)).setError("Valor requerido");
+            }
+            Toast.makeText(this, "Falha ao calcular", Toast.LENGTH_SHORT).show();
+        }
+
     }
     public void Sell (View view){
         Market market = new Market();
-
-        double calculo = market.Sell(Double.parseDouble(((EditText) findViewById(R.id.edSellQuantidadeCoinDisponivel)).getText().toString()), Double.parseDouble(((EditText) findViewById(R.id.edSellGoalCoinValor)).getText().toString()), 0.1);
-        ((TextView) findViewById(R.id.edSellGoalCoinSaldo)).setText(String.valueOf(df4.format(calculo)));
-        Toast.makeText(this, String.valueOf(calculo), Toast.LENGTH_SHORT).show();
+        try {
+            double calculo = market.Sell(Double.parseDouble(((EditText) findViewById(R.id.edSellQuantidadeCoinDisponivel)).getText().toString()), Double.parseDouble(((EditText) findViewById(R.id.edSellGoalCoinValor)).getText().toString()), 0.1);
+            ((TextView) findViewById(R.id.edSellGoalCoinSaldo)).setText(String.valueOf(df4.format(calculo)));
+            Toast.makeText(this, String.valueOf(calculo), Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e){
+            if(((EditText) findViewById(R.id.edSellQuantidadeCoinDisponivel)).getText().toString().equals("")){
+                ((EditText) findViewById(R.id.edSellQuantidadeCoinDisponivel)).setError("Valor requerido");
+            }
+            if(((EditText) findViewById(R.id.edSellGoalCoinValor)).getText().toString().equals("")){
+                ((EditText) findViewById(R.id.edSellGoalCoinValor)).setError("Valor requerido");
+            }
+            Toast.makeText(this, "Falha ao calcular", Toast.LENGTH_SHORT).show();
+        }
     }
 }
