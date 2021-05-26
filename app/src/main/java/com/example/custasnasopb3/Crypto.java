f70199e44fe1d209aeb55dae7eda837a5ce3266f
Gabriel Carneiro <gabucarneiro@gmail.com>
2021-05-25 23:44:57 -0300

    ESTADO COMPLETO*: APARENTE - Falta melhorar o código e o consumo de memória do aparelho. Foi alterada a função de Buy na classe Crypto.java para receber os valores e já calcular o valor mínimo para venda já inserindo na view de "Valor de venda" o valor mínimo para venda - O usuário continua podendo alterar esse valor para realizar novos cálculos. A quantidade de moedas definida na função Buy na mesma classe já é também setada na quantidade disponível.

--- app/src/main/java/com/example/custasnasopb3/Crypto.java
+++ app/src/main/java/com/example/custasnasopb3/Crypto.java
@@ -3,6 +3,7 @@ package com.example.custasnasopb3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -22,6 +23,7 @@ public class Crypto extends AppCompatActivity {
 
     double taxBuy = 0.1;
     double taxExchage = 0.175;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,54 +81,106 @@ public class Crypto extends AppCompatActivity {
 
     public void Buy (View view){
         Market market = new Market();
+        double tempValorVenda;
 
         try {
             try {
                 taxBuy = Double.parseDouble(((EditText)findViewById(R.id.pctCompraCrpt)).getText().toString());
+                //Toast.makeText(this, ("TaxBuy = " + taxBuy), Toast.LENGTH_SHORT).show();
                 try {
                     ((TextView) findViewById(R.id.txCompraCrpt)).setText( df6.format(((Double.parseDouble(((EditText)findViewById(R.id.edBuyCoinDisponivel)).getText().toString())) / (Double.parseDouble(((EditText)findViewById(R.id.edBuyGoalCoinValor)).getText().toString()))) * (taxBuy/100)));
+                    //Toast.makeText(this, ("TaxBuy 2 = " + taxBuy), Toast.LENGTH_SHORT).show();
                 }
                 catch (Exception buy0value){
+                    String yey = buy0value.getMessage();
+                    //Toast.makeText(this, yey, Toast.LENGTH_SHORT).show();
+
                     ((TextView) findViewById(R.id.txCompraCrpt)).setText(String.valueOf(0.0));
+                    Toast.makeText(this, "Exception try não foi possível fazer o cálculo taxBuy", Toast.LENGTH_SHORT).show();
                 }
             }
             catch (Exception e){
                 taxBuy = 0.0;
                 ((EditText)findViewById(R.id.pctCompraCrpt)).setText(String.valueOf(taxBuy));
+                //Toast.makeText(this, ("Catch = " + taxBuy), Toast.LENGTH_SHORT).show();
                 try {
                     ((TextView) findViewById(R.id.txCompraCrpt)).setText( df6.format((Double.parseDouble(((EditText)findViewById(R.id.edBuyCoinDisponivel)).getText().toString())) / (Double.parseDouble(((EditText)findViewById(R.id.edBuyGoalCoinValor)).getText().toString()))));
+                    //Toast.makeText(this, ("Catch try = " + taxBuy), Toast.LENGTH_SHORT).show();
                 }
                 catch (Exception buy0value){
                     ((TextView) findViewById(R.id.txCompraCrpt)).setText(String.valueOf(0.0));
+                    Toast.makeText(this, "Catch novamente taxBuy= 0.0", Toast.LENGTH_SHORT).show();
                 }
             }
             try {
                 taxExchage = Double.parseDouble(((EditText)findViewById(R.id.pctTransacaoCrpt)).getText().toString());
+                //Toast.makeText(this, ("TaxExchange = " + taxExchage), Toast.LENGTH_SHORT).show();
                 try {
                     ((TextView) findViewById(R.id.TransacaoCrpt)).setText( df6.format(((Double.parseDouble(((EditText)findViewById(R.id.edBuyCoinDisponivel)).getText().toString())) / (Double.parseDouble(((EditText)findViewById(R.id.edBuyGoalCoinValor)).getText().toString()))) * (taxExchage/100)));
+                    //Toast.makeText(this, ("TaxExchange 2 = " + taxExchage), Toast.LENGTH_SHORT).show();
                 }
                 catch (Exception buy0value){
                     ((TextView) findViewById(R.id.TransacaoCrpt)).setText(String.valueOf(0.0));
+                    Toast.makeText(this, "Exception try não foi possível fazer o cálculo Exchange", Toast.LENGTH_SHORT).show();
                 }
             }
             catch (Exception e){
                 taxExchage = 0.0;
                 ((EditText)findViewById(R.id.pctTransacaoCrpt)).setText(String.valueOf(taxExchage));
+                Toast.makeText(this, ("Catch TaxExchange = " + taxExchage), Toast.LENGTH_SHORT).show();
                 try {
                     ((TextView) findViewById(R.id.TransacaoCrpt)).setText( df6.format((Double.parseDouble(((EditText)findViewById(R.id.edBuyCoinDisponivel)).getText().toString())) / (Double.parseDouble(((EditText)findViewById(R.id.edBuyGoalCoinValor)).getText().toString()))));
                 }
                 catch (Exception buy0value){
                     ((TextView) findViewById(R.id.TransacaoCrpt)).setText(String.valueOf(0.0));
+                    Toast.makeText(this, "Catch novamente taxExchange recebe 0.0", Toast.LENGTH_SHORT).show();
                 }
             }
 
             double calculo = market.Buy(Double.parseDouble(((EditText) findViewById(R.id.edBuyCoinDisponivel)).getText().toString()), Double.parseDouble(((EditText) findViewById(R.id.edBuyGoalCoinValor)).getText().toString()), (taxBuy+taxExchage));
             ((TextView) findViewById(R.id.edBuyGoalCoinQuantidade)).setText(df6.format(calculo));
+            ((EditText) findViewById(R.id.edSellQuantidadeCoinDisponivel)).setText(df4.format(calculo));
 
-            Toast.makeText(this, String.valueOf((taxBuy+taxExchage)), Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, String.valueOf(calculo), Toast.LENGTH_SHORT).show();
+            ((EditText) findViewById(R.id.edSellGoalCoinValor)).setText((((EditText) findViewById(R.id.edBuyGoalCoinValor)).getText().toString()));
+            Sell(view);
+
+            /*new Thread(){
+                public void run(){
+                    try {
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                while (Double.parseDouble(((TextView) findViewById(R.id.edSellGoalCoinSaldo)).getText().toString()) < Double.parseDouble(((EditText)findViewById(R.id.edBuyCoinDisponivel)).getText().toString())){
+                                    tempValorVenda = Double.parseDouble(((EditText) findViewById(R.id.edSellGoalCoinValor)).getText().toString()) + 0.001;
+                                    ((EditText)findViewById(R.id.edSellGoalCoinValor)).setText(df4.format(tempValorVenda));
+                                    Sell(view);
+                                }
+                            }
+                        }){
+
+                        }
+                    }
+                    catch (Exception e){
+                        e.getMessage();
+                        e.printStackTrace();
+                    }
+                }
+            }.start();*/
+
+
+            //TODO COLOCAR A FUNÇÃO ABAIXO EM UMA NOVA THREAD
+            while (Double.parseDouble(((TextView) findViewById(R.id.edSellGoalCoinSaldo)).getText().toString()) < Double.parseDouble(((EditText)findViewById(R.id.edBuyCoinDisponivel)).getText().toString())){
+                tempValorVenda = Double.parseDouble(((EditText) findViewById(R.id.edSellGoalCoinValor)).getText().toString()) + 0.001;
+                ((EditText)findViewById(R.id.edSellGoalCoinValor)).setText(df4.format(tempValorVenda));
+                Sell(view);
+            }
+            //TODO COLOCAR A FUNÇÃO ACIMA EM UMA NOVA THREAD
+
+            //Toast.makeText(this, String.valueOf((taxBuy+taxExchage)), Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, String.valueOf(calculo), Toast.LENGTH_SHORT).show();
         }
         catch (Exception e){
+
             if(((EditText) findViewById(R.id.edBuyCoinDisponivel)).getText().toString().equals("")){
                 ((EditText) findViewById(R.id.edBuyCoinDisponivel)).setError("Valor requerido");
             }
@@ -169,7 +223,7 @@ public class Crypto extends AppCompatActivity {
             }
             double calculo = market.Sell(Double.parseDouble(((EditText) findViewById(R.id.edSellQuantidadeCoinDisponivel)).getText().toString()), Double.parseDouble(((EditText) findViewById(R.id.edSellGoalCoinValor)).getText().toString()), (taxBuy+taxExchage));
             ((TextView) findViewById(R.id.edSellGoalCoinSaldo)).setText(df4.format(calculo));
-            Toast.makeText(this, String.valueOf(calculo), Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, String.valueOf(calculo), Toast.LENGTH_SHORT).show();
         }
         catch (Exception e){
             if(((EditText) findViewById(R.id.edSellQuantidadeCoinDisponivel)).getText().toString().equals("")){
