16186b0617bf2a271f09aea273d52e6e6abcab70
Gabriel Carneiro <gabucarneiro@gmail.com>
2021-06-09 23:22:39 -0300

    Criado apenas a classe para exportar os dados do banco de dados para um arquivo TXT cuja leitura poderá ser feita em qualquer editor de texto.

---
+++ app/src/main/java/com/example/custasnasopb3/Export.java
@@ -0,0 +1,11 @@
+package com.example.custasnasopb3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Export extends AppCompatActivity {
+
+
+    public void Exportar(){
+
+    }
+}
