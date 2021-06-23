118791b2d33b714f3cd2bd39ca335e1f0929a770
Gabriel Carneiro <gabucarneiro@gmail.com>
2021-06-22 23:36:03 -0300

    Fluindo, mas ainda com erro: fw = new FileWriter(f.getName(), true);                                     //Attempt to invoke virtual method 'java.io.Writer java.io.FileWriter.append(char)' on a null object reference                                     //TENTAR GERAR DIRETAMENTE O ARQUIVO E REALIZAR O APPEND NO INICIO DO CÓDIGO

--- app/src/main/java/com/example/custasnasopb3/Export.java
+++ app/src/main/java/com/example/custasnasopb3/Export.java
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -23,6 +24,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.util.Scanner;
 
@@ -77,9 +79,51 @@ public class Export extends AppCompatActivity {
 
         //sc.close();
     }
+    public void testeCanWrite(View v){
+        //AQUI CONSEGUE ESCREVER!!!!  File temp = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).getPath());
+
+        diretorioApp = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+        //diretorioApp = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+        //Environment.getExternalStorageDirectory().canWrite();
+
+
+        //File temp = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).getPath());
+        File temp = new File(diretorioApp);
+        String canwrite;
+        if (temp.canWrite()){
+            canwrite = "yes!!";
+        }
+        else {
+            canwrite = "NO!";
+        }
+
+
+        /*if (Environment.getExternalStorageDirectory().canWrite()){
+            canwrite = "yes!!";
+        }
+        else {
+            canwrite = "NO!";
+        }*/
+        Toast.makeText(this, canwrite, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, temp.toString(), Toast.LENGTH_SHORT).show();
+    }
+
     public void criarArquivo(View v){
 
-        diretorioApp = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/"+nomeDiretorio+"/";
+        diretorioApp = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+        //diretorioApp = Environment.getExternalStorageDirectory().getAbsolutePath() + "/"+nomeDiretorio+"/";
+//
+//        CÓDIGO ABAIXO CRIADO PARA TESTAR SE ERA POSSÍVEL ESCREVER
+//        File temp = new File(diretorioApp);
+//        String canwrite;
+//        if (temp.canWrite()){
+//            canwrite = "yes!!";
+//        }
+//        else {
+//            canwrite = "NO!";
+//        }
+//        Toast.makeText(this, canwrite, Toast.LENGTH_SHORT).show();
+
 
         diretorio = new File(diretorioApp);
         diretorio.mkdirs();
@@ -90,19 +134,33 @@ public class Export extends AppCompatActivity {
         File fileExt;
         FileOutputStream fosExt = null;
         FileInputStream fisExt = null;
-        FileWriter fw = null;
+        FileWriter fw;
 
         fileExt = new File(diretorioApp, nomeArquivo);
         fileExt.getParentFile().mkdirs();
 
+//        JÁ DEU MERDA LOGO DE INICIO MESMO! KKK
+//        try{
+//            fw = new FileWriter(diretorioApp, true);
+//            fw.append("\n");
+//        }
+//        catch (IOException io){
+//            io.printStackTrace();
+//            Toast.makeText(this, "Deu merda logo no inicio!", Toast.LENGTH_SHORT).show();
+//        }
 
         //Fiz para testar, ainda sem resultados.
-        try {
-            fosExt = openFileOutput("NovoArquivo", MODE_APPEND);
-            fosExt.write("\nwhatnow".getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            //save("whatnow", "NovoArquivo.txt");
+//
+//            fosExt = openFileOutput(nomeArquivo, Context.MODE_APPEND);
+//            fosExt.write("whatnow".getBytes());
+//            fosExt.flush();
+//            fosExt.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//        }
 
 
 
@@ -128,6 +186,12 @@ public class Export extends AppCompatActivity {
             Toast.makeText(this, "Arquivo não encontrado", Toast.LENGTH_SHORT).show();
         }*/
 
+
+
+
+
+
+
         try {
             /*if (((((EditText) findViewById(R.id.edSelectedFile)).getText().toString())).equals(fileExt.getName() + ".txt")){
                 fosExt = openFileOutput(nomeArquivo, MODE_APPEND);
@@ -152,31 +216,80 @@ public class Export extends AppCompatActivity {
                             f.setWritable(true);
                             Toast.makeText(this, ("Achamos um arquivo: " + f.getName()), Toast.LENGTH_SHORT).show();
                             try {
-                                //fw.write(temp, MODE_APPEND);
-                                fosExt = openFileOutput(f.getName(), MODE_APPEND); //Já tentei incluindo o .txt no final
+                                Toast.makeText(this, "FW início", Toast.LENGTH_SHORT).show();
+                                String canwrite2;
+                                if (f.canWrite()){
+                                    canwrite2 = "f CAN WRITE!!";
+                                    if (f.getName()==null){
+                                        Toast.makeText(this, "F.getName é null!!", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else {
+
+                                        Toast.makeText(this, "F.getName NÃO é null!!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                                else {
+                                    canwrite2 = "f não pode ser gravado!";
+                                }
+                                Toast.makeText(this, canwrite2, Toast.LENGTH_SHORT).show();
+                                try {
+                                    fw = new FileWriter(f.getName(), true);
+                                    //Attempt to invoke virtual method 'java.io.Writer java.io.FileWriter.append(char)' on a null object reference
+                                    //TENTAR GERAR DIRETAMENTE O ARQUIVO E REALIZAR O APPEND NO INICIO DO CÓDIGO
+                                }
+                                catch (Exception e){
+                                    e.printStackTrace();
+                                    fw = new FileWriter("LOG_DE_ERRO", true);
+                                    fw.append(e.getMessage());
+                                    fw.flush();
+                                    fw.close();
+                                }
+                                String yeah = "owyear!";
+                                char yeahr = yeah.charAt(1);
+                                Toast.makeText(this, "FW meio", Toast.LENGTH_SHORT).show();
+                                fw.append(yeahr);
+                                fw.append(yeahr);
+                                fw.append(yeahr);
+//                                fw.append("GOD BLESS IT!");
+//                                fw.append("GOD BLESS IT!");
+//                                fw.append("GOD BLESS IT!");
+                                Toast.makeText(this, "FW fim", Toast.LENGTH_SHORT).show();
+                                fw.flush();
+                                fw.close();
+
+                                Toast.makeText(this, "FW testado", Toast.LENGTH_SHORT).show();
+
+                                /*fosExt = openFileOutput(f.getName(), Context.MODE_APPEND); //Já tentei incluindo o .txt no final
                                 fosExt.write((((EditText)findViewById(R.id.edListar)).getText().toString()).getBytes());
+                                fosExt.flush();
+                                fosExt.close();*/
 
-                                /*Não funciona
-                                openFileOutput(f.getName(), MODE_APPEND).write((((EditText)findViewById(R.id.edListar)).getText().toString()).getBytes());*/
 
-                                /*Não funciona
-                                fosExt = new FileOutputStream(f.getName(), true);
-                                byte[] edListar = ((EditText)findViewById(R.id.edListar)).getText().toString().getBytes();
-                                fosExt.write(edListar);*/
 
-                                /*Não funciona
-                                fosExt = new FileOutputStream(f.getName(), true);
-                                fosExt.write((((EditText)findViewById(R.id.edListar)).getText().toString()).getBytes());*/
+
+//                                Não funciona
+//                                openFileOutput(f.getName(), MODE_APPEND).write((((EditText)findViewById(R.id.edListar)).getText().toString()).getBytes());
+
+//                                Não funciona
+//                                fosExt = new FileOutputStream(f.getName(), true);
+//                                byte[] edListar = ((EditText)findViewById(R.id.edListar)).getText().toString().getBytes();
+//                                fosExt.write(edListar);
+
+//                                Não funciona
+//                                fosExt = new FileOutputStream(f.getName(), true);
+//                                fosExt.write((((EditText)findViewById(R.id.edListar)).getText().toString()).getBytes());
 
 
                             }
                             catch (IOException ioe){
+                                ioe.printStackTrace();
+                                Toast.makeText(this, ioe.getMessage(), Toast.LENGTH_SHORT).show();
                                 Toast.makeText(this, "IOException", Toast.LENGTH_SHORT).show();
                             }
                             //fw.append(((EditText)findViewById(R.id.edListar)).getText().toString());
                             //fosExt.write(("\n").getBytes());
-                            /*Toast.makeText(this, ((EditText)findViewById(R.id.edListar)).getText().toString(), Toast.LENGTH_SHORT).show();
-                            Toast.makeText(this, (((EditText)findViewById(R.id.edListar)).getText().toString().getBytes()).toString(), Toast.LENGTH_SHORT).show();*/
+                            //Toast.makeText(this, ((EditText)findViewById(R.id.edListar)).getText().toString(), Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(this, (((EditText)findViewById(R.id.edListar)).getText().toString().getBytes()).toString(), Toast.LENGTH_SHORT).show();
                             //fosExt.write(((EditText)findViewById(R.id.edListar)).getText().toString().getBytes());
                             //fosExt.write(mybytes);
 
@@ -187,12 +300,12 @@ public class Export extends AppCompatActivity {
                             Toast.makeText(this, i +" - fExistente = "+ fExistente, Toast.LENGTH_SHORT).show();
                         }
                         /*((TextView)findViewById(R.id.txtaArquivosListados)).append("\n");
-                        ((TextView)findViewById(R.id.txtaArquivosListados)).setText(f.getName());
-                        Toast.makeText(this, (f.getName()), Toast.LENGTH_SHORT).show();*/
+                        ((TextView)findViewById(R.id.txtaArquivosListados)).setText(f.getName());*/
+                        //Toast.makeText(this, (f.getName()), Toast.LENGTH_SHORT).show();
                     }
                 }
                 if (fExistente == 0){
-                    Toast.makeText(this, "If fora do array!", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(this, "If fora do array!", Toast.LENGTH_SHORT).show();
                     fosExt = new FileOutputStream(fileExt);
                     fosExt.write(((EditText)findViewById(R.id.edListar)).getText().toString().getBytes());
                     Toast.makeText(this, "Novo arquivo escrito!", Toast.LENGTH_SHORT).show();
@@ -202,44 +315,73 @@ public class Export extends AppCompatActivity {
                 Toast.makeText(this, "Pasta vazia!", Toast.LENGTH_SHORT).show();
                 fosExt = new FileOutputStream(fileExt);
                 fosExt.write(((EditText)findViewById(R.id.edListar)).getText().toString().getBytes());
-                Toast.makeText(this, "Novo arquivo escrito!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Novo arquivo criado!", Toast.LENGTH_SHORT).show();
             }
 
             /*fosExt = new FileOutputStream(fileExt);
             fosExt.write(((EditText)findViewById(R.id.edListar)).getText().toString().getBytes());
             Toast.makeText(this, "Escrivinhado!", Toast.LENGTH_SHORT).show();
             fosExt.close();*/
-            fosExt.close();
             Listar(findViewById(R.id.arquivosListados));
         }
         catch (Exception e){
             Toast.makeText(this, "Não foi possível escrever", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+            ((EditText)findViewById(R.id.edListar)).setText(e.getMessage());
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
-        /*try {
+        try {
             fosExt.close();
         }
         catch (Exception e){
-            Toast.makeText(this, "Não foi possível fechar", Toast.LENGTH_SHORT).show();
-        }*/
-
-        /*File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/" + nomeDiretorio + "/", "RecebaEssetxt");
-        FileOutputStream fileOutputStream;
-
-        try {
-            fileOutputStream = new FileOutputStream(file);
-            fileOutputStream.write("string".getBytes());
-            fileOutputStream.close();
-            file.setExecutable(true);
-            file.setReadable(true);
-            file.setWritable(true);
-
-            MediaScannerConnection.scanFile(this, new String[]{file.getAbsolutePath()}, null, null);
-        }
-        catch (Exception e){
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
             e.printStackTrace();
-        }*/
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Não foi possível fechar", Toast.LENGTH_SHORT).show();
+        }
+
+//        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/" + nomeDiretorio + "/", "RecebaEssetxt");
+//        FileOutputStream fileOutputStream;
+//
+//        try {
+//            fileOutputStream = new FileOutputStream(file);
+//            fileOutputStream.write("string".getBytes());
+//            fileOutputStream.close();
+//            file.setExecutable(true);
+//            file.setReadable(true);
+//            file.setWritable(true);
+//
+//            MediaScannerConnection.scanFile(this, new String[]{file.getAbsolutePath()}, null, null);
+//        }
+//        catch (Exception e){
+//            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//            e.printStackTrace();
+//        }
     }
+
+
+
+    private void save(String data, String fileName){
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try {
+            out = getApplicationContext().openFileOutput(fileName, Context.MODE_APPEND);
+            byte[] bytes = data.getBytes();
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(data);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (writer!=null){writer.close();}
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void Listar(View v){
         LinearLayout LL = findViewById(R.id.arquivosListados);
         LL.setOrientation(LinearLayout.VERTICAL);
